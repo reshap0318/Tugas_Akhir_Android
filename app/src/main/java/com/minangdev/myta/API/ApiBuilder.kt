a56e23f5ed3aedf59d4066c04cbfd302d330f9e1
reshap0318 <aldo.reshap@gmail.com>
2020-10-30 23:42:50 +0700

    crud news

---
+++ app/src/main/java/com/minangdev/myta/API/ApiBuilder.kt
@@ -0,0 +1,22 @@
+package com.minangdev.myta.API
+
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object ApiBuilder {
+    // #kampus : http://192.168.0.181:8000 #home : http://192.168.100.81:8000 #heroku : https://akhir-tugas.herokuapp.com/
+    private const val BASE_URL = "http://192.168.100.81:8000"
+
+    private val okHttp = OkHttpClient.Builder()
+
+    private val builder = Retrofit.Builder().baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttp.build())
+
+    private val retrofit = builder.build()
+
+    fun <T> buildService(serviceType: Class<T>): T{
+        return retrofit.create(serviceType)
+    }
+}
--- app/src/main/java/com/minangdev/myta/View/MainActivity.kt
+++ app/src/main/java/com/minangdev/myta/View/MainActivity.kt
@@ -7,6 +7,7 @@ import android.widget.FrameLayout
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.minangdev.myta.Helper.SharePreferenceManager
 import com.minangdev.myta.R
 import com.minangdev.myta.View.annaouncement.AnnouncementFragment
 import com.minangdev.myta.View.home.HomeFragment
@@ -18,37 +19,70 @@ class MainActivity : AppCompatActivity() {
 
     private var mFrameLayout: FrameLayout? = null
 
+    companion object {
+        const val EXTRA_FRAGMENT = "extra_fragment"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        bottom_navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
 
+        val sharePreference = SharePreferenceManager(this)
+        sharePreference.isLogin()
+
+        bottom_navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
         menu_notification.setOnClickListener{
-            val intent = Intent(this, notificationActivity::class.java)
+            val intent = Intent(this, NotificationActivity::class.java)
             startActivity(intent)
         }
+        val idFragment = intent.getIntExtra(EXTRA_FRAGMENT, 0)
+        if(idFragment>0){
+            changeFragment(idFragment)
+        }else{
+            changeFragment(1)
+        }
 
-        val fragment = HomeFragment.newInstance()
-        addFragment(fragment)
     }
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_home -> {
-                val fragment = HomeFragment.newInstance()
-                addFragment(fragment)
+                changeFragment(1)
                 true
             }
             R.id.navigation_announcement -> {
+                changeFragment(2)
+                true
+            }
+            R.id.navigation_topic -> {
+                Toast.makeText(this, "Topic", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.navigation_profile -> {
+                changeFragment(4)
+                true
+            }
+            else -> false
+        }
+    }
+
+    private fun changeFragment(idFragment:Int){
+        when(idFragment){
+            1 -> {
+                val fragment = HomeFragment()
+                addFragment(fragment)
+                true
+            }
+            2 -> {
                 val fragment = AnnouncementFragment()
                 addFragment(fragment)
                 true
             }
-            R.id.navigation_resource -> {
-                Toast.makeText(this, "resource", Toast.LENGTH_SHORT).show()
+            3 -> {
+                Toast.makeText(this, "Topic", Toast.LENGTH_SHORT).show()
                 true
             }
-            R.id.navigation_profile -> {
+            4 -> {
                 val fragment = ProfileFragment()
                 addFragment(fragment)
                 true
